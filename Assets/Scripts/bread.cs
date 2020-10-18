a9408323bf943d02aa31c44c80ded9090d572be5
y-solb <ysolb99@gmail.com>
2020-10-18 19:11:00 +0900

    ediya_ysb

---
+++ Assets/Scripts/bread.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bread : MonoBehaviour
+{
+    public GameObject obj;
+
+    public void BreadOnButton()
+    {
+        obj.SetActive(true);
+    }
+
+    public void BreadOffButton()
+    {
+        obj.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/nextbefore.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+
+
+ 
+public class nextbefore : MonoBehaviour
+{
+    public Sprite[] gallery; //store all your images in here at design time
+    public Image displayImage; //The current image thats visible
+    public Button nextImg; //Button to view next image
+    public Button prevImg; //Button to view previous image
+    public int i = 0; //Will control where in the array you are
+
+    public GameObject obj;
+
+    public void BreadOnButton()
+    {
+        obj.SetActive(true);
+    }
+
+    public void BreadOffButton()
+    {
+            obj.SetActive(false);
+    }
+
+
+    public void BtnNext()
+    {
+        if (i + 1 < gallery.Length)
+        {
+            i++;
+        }
+    }
+
+    public void BtnPrev()
+    {
+        if (i - 1 > -1)
+        {
+            i--;
+        }
+     
+    }
+
+    void Update()
+    {
+        displayImage.sprite = gallery[i];
+    }
+
+}
